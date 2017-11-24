7a5f7d31a6cb5088d5c826e4b0567d310c37bcd7
Marin Godechot <marin.godechot@yahoo.fr>
2017-11-24 11:19:50 +0100

    Create Main class

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+	static Scanner scan = new Scanner(System.in);
+
+
+	public static void main(String[] args) {
+		System.out.println("Entrez une phrase :");
+		String str = scan.nextLine();
+		str = str.replaceAll("[^a-zA-Z]", "");
+		System.out.println(isPalindrome(str) ? "C'est un palindrome" : "Ce n'est pas un palindrome");
+	}
+	
+	public static boolean isPalindrome (String phrase) {
+		int a=0; int b = phrase.length()-1;
+		while (a<b){
+			if (phrase.substring(a, a).equals(phrase.substring(b, b))){
+				a++; b--;
+			} else {
+				return false;
+			}
+		}
+		return true;
+	}
+
+}
